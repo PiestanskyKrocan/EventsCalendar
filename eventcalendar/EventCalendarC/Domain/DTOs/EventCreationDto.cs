4369a52bcdd78f416bbdc4da4760222f134b0f3c
Alzimax <90858641+Alzimax@users.noreply.github.com>
2022-12-16 03:44:51 +0100

    working

--- eventcalendar/EventCalendarC/Domain/DTOs/EventCreationDto.cs
+++ eventcalendar/EventCalendarC/Domain/DTOs/EventCreationDto.cs
@@ -4,10 +4,18 @@ public class EventCreationDto
 {
     public int OwnerId { get; }
     public string Title { get; }
-
-    public EventCreationDto(int ownerId, string title)
+    public string BodyText { get; }
+    public string StartTime { get; }
+    public string EndTime { get; }
+    public string Address { get;}
+    
+    public EventCreationDto(int ownerId, string title, string bodyText, string startTime, string endTime, string address)
     {
         OwnerId = ownerId;
         Title = title;
+        BodyText = bodyText;
+        StartTime = startTime;
+        EndTime = endTime;
+        Address = address;
     }
 }
--- eventcalendar/EventCalendarC/Domain/DTOs/SearchEventsWithParametersDto.cs
+++ eventcalendar/EventCalendarC/Domain/DTOs/SearchEventsWithParametersDto.cs
@@ -6,12 +6,21 @@ public class SearchEventsWithParametersDto
     public int? UserId { get;}
     public bool? CompletedStatus { get;}
     public string? TitleContains { get;}
+    public string? BodyTextContains { get; set; }
+    public string? StartTimeIs { get; set; }
+    public string? EndTimeIs { get; set; }
+    public string? AddressContains { get; set;}
 
-    public SearchEventsWithParametersDto(string? username, int? userId, bool? completedStatus, string? titleContains)
+    public SearchEventsWithParametersDto(string? username, int? userId, bool? completedStatus, string? titleContains, string? bodyTextContains,
+        string? startTimeIs, string? endTimeIs, string? addressContains)
     {
         Username = username;
         UserId = userId;
         CompletedStatus = completedStatus;
         TitleContains = titleContains;
+        BodyTextContains = bodyTextContains;
+        StartTimeIs = startTimeIs;
+        EndTimeIs = endTimeIs;
+        AddressContains = addressContains;
     }
 }
